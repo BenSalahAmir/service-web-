e7cb965ba50c14c6a836d16bdfb683ac9fecd028
BenSalahAmir <amir.bensalah@esprit.tn>
2022-10-30 22:28:35 +0100

    crud graph ql

---
+++ src/main/java/graphql/GraphQLEndpoint.java
@@ -0,0 +1,30 @@
+package graphql;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.coxautodev.graphql.tools.Resolver;
+import com.coxautodev.graphql.tools.SchemaParser;
+
+import graphql.schema.GraphQLSchema;
+import graphql.servlet.SimpleGraphQLServlet;
+import repository.LogementRepository;
+import repository.RendezVousRepository;
+
+@WebServlet(urlPatterns = "/graphql")
+public class GraphQLEndpoint extends SimpleGraphQLServlet {
+
+	
+	  public GraphQLEndpoint(){
+		 super(buildSchema());
+	    }
+
+	    private static GraphQLSchema buildSchema() {
+	    	LogementRepository lr = new LogementRepository();
+	    	RendezVousRepository rdvRepo = new RendezVousRepository();
+	        return SchemaParser.newParser()
+	                .file("schema.graphqls")
+	                .resolvers(new Query(lr,rdvRepo),new Mutation(lr,rdvRepo))
+	                .build()
+	                .makeExecutableSchema();
+	    }
+}
